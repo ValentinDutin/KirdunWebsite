a42bdd04c6deb84bd8d510cc2ae4f43d9fbcdf04
ValentinDutin <valentin.dutin@gmail.com>
2022-02-05 04:59:23 +0300

    Youtube icon is changed

--- src/components/MovieBlock.vue
+++ src/components/MovieBlock.vue
@@ -12,6 +12,7 @@
     >
     </iframe> -->
     <!-- allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" -->
+    <!-- ▶ -->
     <iframe class="movie"
       :src="moviePath"
       :srcdoc="srcdoc"
@@ -53,15 +54,25 @@ export default {
     },
     createSrcdoc () {
       this.moviePath.concat('?autoplay=1')
-      this.srcdoc = `<style>*{padding:0;margin:0;overflow:hidden;}
+      this.srcdoc = `
+        <style>*{padding:0;margin:0;overflow:hidden;}
         img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}
-        span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style>
+        svg{ width: 68px; text-align:center;color:rgba(30, 30, 30, 0.8);}
+        span{height:1.5em;text-align:center;font:45px/1.5 sans-serif;}</style>
         <a href="` + this.moviePath + `">
-        <img src="${this.createImgPath()}"
-        alt="` + this.title + `"
+            <img src="${this.createImgPath()}"
+            alt="` + this.title + `"
         >
-        <span>▶</span>
-        </a>`
+          <span>
+            <svg height="100%" viewBox="0 0 68 48" width="100%">
+              <path d="M66.52,7.74c-0.78-2.93-2.49-5.41-5.42-6.19C55.79,.13,34,0,34,0S12.21,.13,6.9,1.55 C3.97,2.33,2.27,4.81,1.48,7.74C0.06,13.05,0,24,0,24s0.06,10.95,1.48,16.26c0.78,2.93,2.49,5.41,5.42,6.19 C12.21,47.87,34,48,34,48s21.79-0.13,27.1-1.55c2.93-0.78,4.64-3.26,5.42-6.19C67.94,34.95,68,24,68,24S67.94,13.05,66.52,7.74z" fill="currentColor">
+              </path>
+              <path d="M 45,24 27,14 27,34" fill="#fff">
+              </path>
+            </svg>
+          </span>
+        </a>
+      `
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import './assets/layouts/index.scss'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
+// import { faYoutube } from '@fortawesome/free-brands-svg-icons'
 // import { fab } from '@fortawesome/free-brands-svg-icons'
 
 library.add(faBars)
