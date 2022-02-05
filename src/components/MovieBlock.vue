63beabcc46890a8719e410043b7a37884d63c4e0
ValentinDutin <valentin.dutin@gmail.com>
2022-02-05 03:42:08 +0300

    MovieBlock is optimized

--- src/components/MovieBlock.vue
+++ src/components/MovieBlock.vue
@@ -3,14 +3,24 @@
     <h3 class="title">
         {{title}}
     </h3>
-    <iframe class="movie"
+    <!-- <iframe class="movie"
         :src="moviePath"
-        title="YouTube video player"
+        :title="title"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowfullscreen
     >
-    </iframe>
+    </iframe> -->
+    <!-- allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" -->
+    <iframe class="movie"
+      :src="moviePath"
+      :srcdoc="srcdoc"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+      :title="title"
+  >
+</iframe>
 </div>
 </template>
 
@@ -20,6 +30,39 @@ export default {
   props: {
     title: String,
     moviePath: String
+  },
+  data () {
+    return {
+      srcdoc: {
+        type: String
+      }
+    }
+  },
+  created () {
+    this.createSrcdoc()
+  },
+  methods: {
+    createImgPath () {
+      var tpmPath = this.moviePath
+      console.log(this.moviePath)
+      var imgPath = ''
+      var tmp = tpmPath.split('/')
+      imgPath = 'https://i.ytimg.com/vi/' + tmp[tmp.length - 1] + '/hqdefault.jpg'
+      console.log(tmp[0])
+      return imgPath
+    },
+    createSrcdoc () {
+      this.moviePath.concat('?autoplay=1')
+      this.srcdoc = `<style>*{padding:0;margin:0;overflow:hidden;}
+        img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}
+        span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style>
+        <a href="` + this.moviePath + `">
+        <img src="${this.createImgPath()}"
+        alt="` + this.title + `"
+        >
+        <span>▶</span>
+        </a>`
+    }
   }
 }
 </script>
@@ -30,7 +73,6 @@ export default {
     display: flex;
     flex-direction: column;
     justify-content: center;
-    // align-items: center;
     border-radius: 15px;
     background: white;
     height: auto;
